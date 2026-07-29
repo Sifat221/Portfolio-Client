4cab969e87a7c42c12a34c6be2b2b46ed7a77d13
Sifat Khan <170780946+Sifat221@users.noreply.github.com>
2026-07-29 21:23:40 +0600

    feat(profile): update Hero banner image to Profile.JPG

--- src/services/api.ts
+++ src/services/api.ts
@@ -32,8 +32,8 @@ export const defaultPersonal: IPersonalProfile = {
   resumeUrl: "https://sifat221.github.io/SifatKhan-portfolio/",
   github: "https://github.com/Sifat221",
   portfolio: "https://sifat221.github.io/SifatKhan-portfolio/",
-  profilePhoto: "/BannerProfile.JPG",
-  bannerPhoto: "/BannerProfile.JPG",
+  profilePhoto: "/Profile.JPG",
+  bannerPhoto: "/Profile.JPG",
   linkedin: "https://www.linkedin.com/in/sifat-khan-540a86351/",
   facebook: "https://facebook.com/sifatk4an.joy"
 };
